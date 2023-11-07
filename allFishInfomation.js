9fb9d7ced7981614dc43ef43d274e47037567da4
Suematsu-tetsu <146337662+Suematsu-tetsu@users.noreply.github.com>
2023-11-08 03:42:18 +0900

    Create allFishInfomation.js

---
+++ allFishInfomation.js
@@ -0,0 +1,26 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+const fishInfo = [
+  {
+    Number: "001:石津浜",
+    Location: "shizuoka",
+    Fish: ["aji", "iwashi", "ika"],
+    Parking: "with",
+    Address: "静岡県焼津市"
+  },
+  {
+    Number: "002:焼津港",
+    Location: "shizuoka",
+    Fish: ["aji", "iwashi"],
+    Parking: "with",
+    Address: "静岡県焼津市"
+  },
+  {
+    Number: "003:日間賀島",
+    Location: "aichi",
+    Fish: ["aji", "tai", "ika"],
+    Parking: "without",
+    Address: "愛知県南知多町"
+  }
+]
